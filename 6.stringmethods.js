60034ce58101707b8f34c85d51f687d6a1285cf0
KHUSHAL KOTHIYA <khushalkothiya100@gmail.com>
2024-08-01 08:44:45 +0530

    .

--- 6.stringmethods.js
+++ 6.stringmethods.js
@@ -1,23 +1,78 @@
-//slice method 
+// //slice method 
 
-console.log("hello world!".slice(6))  //world!
-console.log("hello world!".slice(6,11)) //world
+// console.log("hello world!".slice(6))  //world!
+// console.log("hello world!".slice(6,11)) //world
 
-//substring 
+// //substring 
 
-console.log("hello".substring(2))  //llo
-console.log("hello".substring(1,3))  //el
+// console.log("hello".substring(2))  //llo
+// console.log("hello".substring(1,3))  //el
 
-//substr
+// //substr
 
-const str = "pw skills"
+// const str = "pw skills"
 
-console.log(str.substr(1,2));  //w
-console.log(str.substr(2));   //skills
+// console.log(str.substr(1,2));  //w
+// console.log(str.substr(2));   //skills
 
-//replace
+// //replace
 
-console.log("hello world world".replace("world","earth"))  //hello earth world
-console.log("hello world world".replaceAll("world","earth"))  //hello earth earth
+// console.log("hello world world".replace("world","earth"))  //hello earth world
+// console.log("hello world world".replaceAll("world","earth"))  //hello earth earth
 
-//to upper case
+// //to upper case
+
+// console.log("Hello World".toLowerCase());  //hello world
+
+// //concat
+
+// const str1 = "PW";
+// const str2 = "SKILLS"
+
+// console.log(str1.concat("",str2));  // PW SKILLS
+// console.log(str2.concat("",str1));  //SKILLS PW
+
+// //Split
+
+// console.log("hello,world,hello,world".split(","));
+
+// //indexof
+
+// console.log("indexOf".indexOf("O"));  //5
+// console.log("indexOf".indexOf("m"));  //-1
+
+// //startswith 
+
+// console.log("hello world".startsWith("hello"));  //true
+
+// //endswith
+
+// console.log("endswith".endsWith("end"));  //true
+// console.log("endswith".endsWith("with"));  //false
+
+// //search
+
+// let str = "hello pw welcome to the world"
+// let searchTerm = "pw";
+// let result = str.search(searchTerm);
+// console.log('search Term:',searchTerm); //sarch term
+// console.log("result:",result);
+
+// //trim
+
+// console.log("hello world".trim());
+
+//chartAt
+
+// console.log("chartAt".charAt(3));
+
+//at
+
+// console.log("hello".at(1));
+// console.log("hello".at(-1));
+
+//charcodeAt
+
+// const str = "PW SKILLS"
+// const index = 4;
+// console.log("charm code",str.charCodeAt(index));
