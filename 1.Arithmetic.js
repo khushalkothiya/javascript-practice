506c46e2764564fa3549465a1c7959eea8cc8bdf
KHUSHAL KOTHIYA <khushalkothiya100@gmail.com>
2024-08-06 11:50:45 +0530

    .

--- 1.Arithmetic.js
+++ 1.Arithmetic.js
@@ -1,22 +1,47 @@
-var num1 = 100;
-var num2 = 2;
+// var num1 = 100;
+// var num2 = 2;
 
-result1 = num1 + num2
-result2 = num1 - num2
-result3 = num1 * num2
-result4 = num1 / num2
-result5 = num1 % num2
-result6 = num1 ** num2
-result7 = num1 ++
-result7 = num1 --
+// result1 = num1 + num2
+// result2 = num1 - num2
+// result3 = num1 * num2
+// result4 = num1 / num2
+// result5 = num1 % num2
+// result6 = num1 ** num2
+// result7 = num1 ++
+// result7 = num1 --
 
-console.log(result1);
-console.log(result2);
-console.log(result3);
-console.log(result4);
-console.log(result5);
-console.log(result6);
-console.log(result7);
-console.log(result8);
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
+// console.log(result4);
+// console.log(result5);
+// console.log(result6);
+// console.log(result7);
+// console.log(result8);
 
+// a=100
+// b=200
+// c=a+b
+// result_type = typeof c;
+// console.log(result_type)
+
+// a=200;
+// b=100;
+// c=a-b;
+// result_type = typeof c;
+// console.log(result_type)
+
+// var students = 100;
+// students += 50;
+// console.log(students)
+
+// var students = 100;
+// students *= 2;
+// console.log(students)
+
+// str1 = "welcome";
+// num1 = 100;
+// result = num1 + str1;
+// result_type = typeof result;
+// console.log(result_type)
 
--- 2.assignment.js
+++ 2.assignment.js
@@ -1,11 +1,12 @@
 var student = 100;
 
-a=student +=50
-b=student -=50
-c=student *=50
-d=student /=50
-e=student %=50
-f=student **=50
+a=student +=50;
+b=student -=50;
+c=student *=50;
+d=student /=50;
+e=student %=50;
+f=student **=50;
+g=student ==50;
 
 console.log(a);
 console.log(b);
